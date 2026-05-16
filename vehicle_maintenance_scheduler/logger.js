0d5691bc909581d01dc276327b218d83951f0388
Kishore kumar <141652966+Kishore1835@users.noreply.github.com>
2026-05-16 16:59:43 +0530

    same logger.js implement for external service
    Added logging functionality to send logs to an external service.

--- vehicle_maintenance_scheduler/logger.js
+++ vehicle_maintenance_scheduler/logger.js
@@ -1,1 +1,37 @@
+require("dotenv").config();
+const axios = require("axios");
+const TOKEN = process.env.TOKEN;
+async function Log(stack, level, packageName, message) {
+    try {
+        const response = await axios.post(
+            "http://4.224.186.213/evaluation-service/logs",
+            {
+                stack: stack,
+                level: level,
+                package: packageName,
+                message: message
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN.trim()}`,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+
+        console.log("Log created"); 
+        console.log(response.data);
+
+    } catch (error) {
+        console.log("Logging failed");
+
+        if (error.response) {
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+    }
+}
+
+module.exports = Log;
 
