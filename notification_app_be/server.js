15dd88c812877b62eae1db041f904490d4f62e32
Kishore kumar <141652966+Kishore1835@users.noreply.github.com>
2026-05-16 17:35:45 +0530

    Implement notification CRUD operations in server.js

--- notification_app_be/server.js
+++ notification_app_be/server.js
@@ -1,1 +1,62 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+const notifications = [
+    {
+        id: 1,
+        type: "Placement",
+        message: "Company hiring drive",
+        read: false
+    },
+    {
+        id: 2,
+        type: "Result",
+        message: "Mid sem results published",
+        read: true
+    }
+];
+
+app.get("/notifications", (req, res) => {
+    res.json(notifications);
+});
+
+app.post("/notifications", (req, res) => {
+
+    const notification = req.body;
+
+    notifications.push(notification);
+
+    res.json({
+        message: "Notification added",
+        notification
+    });
+});
+
+app.put("/notifications/:id/read", (req, res) => {
+
+    const id = parseInt(req.params.id);
+
+    const notification = notifications.find(n => n.id === id);
+
+    if (notification) {
+        notification.read = true;
+    }
+
+    res.json({
+        message: "Notification marked as read"
+    });
+});
+
+app.delete("/notifications/:id", (req, res) => {
+
+    res.json({
+        message: "Notification deleted"
+    });
+});
+
+app.listen(4000, () => {
+    console.log("Notification server running on port 4000");
+});
