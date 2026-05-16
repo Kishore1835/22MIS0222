5c4924d86e5c32e54469f58ba22e612a7eb22ec1
Kishore kumar <141652966+Kishore1835@users.noreply.github.com>
2026-05-16 17:08:28 +0530

    Implement vehicle maintenance scheduling API

--- vehicle_maintenance_scheduler/server.js
+++ vehicle_maintenance_scheduler/server.js
@@ -1,1 +1,73 @@
+require("dotenv").config();
 
+const express = require("express");
+const axios = require("axios");
+
+const optimizeTasks = require("./scheduler");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+const TOKEN = process.env.TOKEN;
+
+app.get("/schedule", async (req, res) => {
+
+    try {
+
+        const depotResponse = await axios.get(
+            "http://4.224.186.213/evaluation-service/depots",
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`
+                }
+            }
+        );
+
+        const vehicleResponse = await axios.get(
+            "http://4.224.186.213/evaluation-service/vehicles",
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`
+                }
+            }
+        );
+
+        const depots = depotResponse.data.depots;
+        const vehicles = vehicleResponse.data.vehicles;
+
+        const result = [];
+
+        for (let depot of depots) {
+
+            const optimized = optimizeTasks(
+                vehicles,
+                depot.MechanicHours
+            );
+
+            result.push({
+                depotId: depot.ID,
+                mechanicHours: depot.MechanicHours,
+                selectedTasks: optimized.selected,
+                totalimpact: optimized.totalimpact,
+                totalhours: optimized.totalhours
+            });
+        }
+
+        res.json(result);
+
+    } catch (error) {
+
+        console.log(error.message);
+
+        res.status(500).json({
+            message: "Error fetching data"
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
